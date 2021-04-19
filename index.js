686125e031e0a26fbb2c3008776b43e86824475b
Pawan Soni <pawansoni76@gmail.com>
2021-04-20 00:00:56 +0530

    Updation

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 
+const HOST = 3000;
+
 app.get('/', (req, res) => {
     res.status(200).send({body: 'Hi From Docker', statusCode: 200})
 })
 
-app.listen(3000, (err) => {
-    if (!err) console.log('listening on 3000');
+app.listen(HOST, (err) => {
+    if (!err) console.log(`listening on ${HOST}`);
     else console.log('Error', JSON.stringify(e))
 })
